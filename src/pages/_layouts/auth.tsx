54c3364eeafe54825f81a5d3a2921fb4375d890e
Alexandre Tonin <xandetonin@gmail.com>
2024-12-25 15:19:23 -0300

    feat: criando página de login e auth layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Lock } from "lucide-react";
+import { Link, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div>
-      <span>Auth Layout</span>
-      <Outlet />
-    </div>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link
+          to="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <Lock className="w-6 h-6 mr-2" />
+          Zustand Auth Flow
+        </Link>
+        <Outlet />
+      </div>
+    </section>
   );
 };
 
